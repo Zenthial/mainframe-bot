e5ec01c0ee613306308a525da9453d51447f97b5
Thomas Schollenberger <tom@schollenbergers.com>
2022-05-14 00:02:49 -0400

    restructure

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,35 @@
-import { REST } from "@discordjs/rest"
-import { Routes } from "discord-api-types/v9"
 import { Client, Collection, Intents } from "discord.js"
 import dotenv from "dotenv"
-import fs from "node:fs"
-import path from "node:path"
-import { CommandInterface } from "./types/CommandInterface"
-
-const commands = new Array();
-const commandsCollection = new Collection<string, CommandInterface>();
-const commandsPath = path.join(__dirname, 'commands')
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
-
-// Place your client and guild ids here
-const clientId = "748886624586432532";
-const guildId = "136517364446527489";
-
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command: CommandInterface = require(filePath).Command;
-    commands.push(command.data.toJSON());
-    commandsCollection.set(command.data.name, command)
-}
+import { load_slash_commands } from "./slash_command_loader"
+import { CommandInterface } from "./types/command_interface"
 
 dotenv.config()
-const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN!);
 
-(async () => {
-    try {
-        console.log("Started refreshing application (/) commands.");
+async function main() {
+    const commandsCollection: Collection<string, CommandInterface> = await load_slash_commands()
 
-        await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands },
-        );
+    const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
-        console.log("Successfully reloaded application (/) commands.");
-    } catch (error) {
-        console.error(error);
-    }
-})();
+    client.on("ready", () => {
+        console.log(`Client logged in`)
+    })
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+    client.on("interactionCreate", async interaction => {
+        if (!interaction.isCommand()) return;
 
-client.on("ready", () => {
-    console.log(`Client logged in`)
-})
+        const command = commandsCollection.get(interaction.commandName);
 
-client.on("interactionCreate", async interaction => {
-    if (!interaction.isCommand()) return;
+        if (!command) return;
 
-    const command = commandsCollection.get(interaction.commandName);
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    })
 
-    if (!command) return;
+    client.login(process.env.DISCORD_TOKEN)
+}
 
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-})
-
-client.login(process.env.DISCORD_TOKEN)
+main()
---
+++ src/slash_command_loader.ts
@@ -0,0 +1,39 @@
+import { REST } from "@discordjs/rest"
+import { Routes } from "discord-api-types/v9"
+import { Collection } from "discord.js"
+import fs from "node:fs"
+import path from "node:path"
+import { CommandInterface } from "./types/command_interface"
+
+export async function load_slash_commands(): Promise<Collection<string, CommandInterface>> {
+    const commands = new Array();
+    const commandsCollection = new Collection<string, CommandInterface>();
+    const commandsPath = path.join(__dirname, 'commands')
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command: CommandInterface = require(filePath).Command;
+        commands.push(command.data.toJSON());
+        commandsCollection.set(command.data.name, command)
+    }
+
+    const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN!);
+
+    await (async () => {
+        try {
+            console.log("Started refreshing application (/) commands.");
+
+            await rest.put(
+                Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!),
+                { body: commands },
+            );
+
+            console.log("Successfully reloaded application (/) commands.");
+        } catch (error) {
+            console.error(error);
+        }
+    })();
+
+    return commandsCollection
+}
